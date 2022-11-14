5f89d1df78d1462a54554165b0ba859e4141ccb9
RangerRhino23 <ryadahlke@outlook.com>
2022-11-14 13:09:15 -0600

    modified:   main.py 	new file:   menu.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from ursina import *
+
+app = Ursina()
+
+def update():
+    pass
+
+def input(key):
+    if key == 'q':
+        quit()
+
+
+
+
+app.run()
---
+++ menu.py
@@ -0,0 +1,25 @@
+from ursina import *
+
+app = Ursina()
+Sky(texture='assets/menu.png')
+
+
+def update():
+    pass
+
+def input(key):
+    if key == 'q':
+        quit()
+
+def select_level():
+    pass
+
+def quit_game():
+    app.closeWindow()
+
+logo = Entity(model='quad', texture='assets/earth.png', scale=(4,2), y = 0.1)
+
+start_button = Button(scale=(.875*.24, .05), x=(logo.x-0.05), y=(logo.y-0.2), text='Start Game', on_click=select_level)
+quit_button = Button(scale=(.875*.24, .05), x=(start_button.x), y=(start_button.y-0.06), text='Quit', on_click=quit_game)
+
+app.run()
